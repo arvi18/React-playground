63ce77cb726a05a3bbdbb4daea27680b557edb1f
Arvind Shelke <ucdonor5@gmail.com>
2021-09-13 13:23:21 +0530

    useEffect hook, localstorge

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,15 +1,25 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AddContact from "./AddContact"
 import ContactList from "./ContactList"
 import Header from './Header';
 
 function App() {
   const [contacts, setContacts]=useState([]);
+  const LOCAL_STORAGE_KEY="contacts";
 
   const addContactHandler=(contact)=>{
     setContacts([...contacts, contact]);
   }
+  
+  useEffect(()=>{
+    const retrieveContacts=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) setContacts(retrieveContacts);
+  }, []);
+
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
   return (
     <div className="ui container" >
       <Header /> 
